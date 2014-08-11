f0983f2d88ad4c8eaf96b775fad6920abe8b6d88
Greg Simon <gregsimon@gmail.com>
2014-08-10 22:39:33 -0700

    can switch banks

--- js/integra7.js
+++ js/integra7.js
@@ -5,7 +5,7 @@ var midiOut = null;
 var selectMIDIIn = null;
 var selectMIDIOut = null;
 var deviceId = 17;
-
+var studioSetControlChannel = 16;
 
 function integra7_init() {
 	if (navigator.requestMIDIAccess)
@@ -16,8 +16,90 @@ function midiMessageReceived( e ) {
 	console.log("MIDI MESSAGE IN "+e);
 }
 
+/*
+SN-A
+ BANK SELECT     | PROGRAM   | GROUP                   | NUMBER
+ MSB | LSB       | NUMBER    |                         | 
+-----+-----------+-----------+----------------------------+-------------
+ 089 | 000 - 001 | 001 - 128 | User SN Acoustic Tone    | 0001 - 0256
+-----+-----------+-----------+----------------------------+-------------
+ 089 | 064 - 065 | 001 - 128 | Preset SN Acoustic Tone  | 0001 - 0256
+
+SN-S
+ BANK SELECT     | PROGRAM   | GROUP                   | NUMBER
+ MSB | LSB       | NUMBER    |                         | 
+-----+-----------+-----------+----------------------------+-------------
+ 095 | 000 - 003 | 001 - 128 | User SN Synth Tone | 0001 - 0512
+-----+-----------+-----------+----------------------------+-------------
+ 095 | 064 | 001 - 128 | Preset SN Synth Tone | 0001 - 0128
+ | : | : | | : 
+ 095 | 072 | 001 - 085 | | 1025 - 1109
+
+ SN-D
+ BANK SELECT     | PROGRAM   | GROUP                   | NUMBER
+ MSB | LSB       | NUMBER    |                         | 
+-----+-----------+-----------+----------------------------+-------------
+ 088 | 000 | 001 - 064 | User SN Drum Kit | 0001 - 0064
+-----+-----------+-----------+----------------------------+-------------
+ 088 | 064 | 001 - 026 | Preset SN Drum Kit | 0001 - 0026
+
+ PCM-S
+ BANK SELECT     | PROGRAM   | GROUP                   | NUMBER
+ MSB | LSB       | NUMBER    |                         | 
+-----+-----------+-----------+----------------------------+-------------
+ 087 | 000 - 001 | 001 - 128 | User PCM Synth Tone | 0001 - 0256
+-----+-----------+-----------+----------------------------+-------------
+ 087 | 064 - 070 | 001 - 128 | Preset PCM Synth Tone | 0001 - 0896
+-----+-----------+-----------+----------------------------+-------------
+ 121 | 000 - | 001 - 128 | GM2 Tone | 0001 - 0256
+
+ PCM-DRUM
+ BANK SELECT     | PROGRAM   | GROUP                   | NUMBER
+ MSB | LSB       | NUMBER    |                         | 
+-----+-----------+-----------+----------------------------+-------------
+ 086 | 000 | 001 - 032 | User PCM Drum Kit | 0001 - 0032
+-----+-----------+-----------+----------------------------+-------------
+ 086 | 064 | 001 - 014 | Preset PCM Drum Kit | 0001 - 0014
+-----+-----------+-----------+----------------------------+-------------
+ 120 | 000 | 001 - 057 | GM2 Drum Kit | 0001 - 0009
+
+*/
+function load_bank(bank) {
+  var ch = 0;
+
+  if (bank == "sna") {
+
+    midiOut.send([0xb0|ch, 0x00, 89, 
+                  0xb0|ch, 0x20, 64]);
+    midiOut.send([0xc0|ch, 45]);
+  }
+  if (bank == "sns") {
+
+    midiOut.send([0xb0|ch, 0x00, 95, 
+                  0xb0|ch, 0x20, 64]);
+    midiOut.send([0xc0|ch, 1]);
+  }
+
+}
+function read_name() {
+  
+}
 function load_part() {
-	sendSYSEXwithRolandChecksum([0x0, 0xa0], [0x0a, 1, 2, 3]);
+  var channel = 1;
+
+  // SN-A
+  // Bank 089 000-001   PN 0x01 - 0x80
+
+
+  
+  //midiOut.send([0x90, 64, 64]);// note on
+  //midiOut.send([0x90, 64, 00]);// note off
+
+
+
+
+
+	//sendSYSEXwithRolandChecksum([0x0, 0xa0], [0x0a, 1, 2, 3]);
 }
 
 // --- MIDI utilities --
@@ -46,7 +128,7 @@ function onMIDIInit( midi ) {
   selectMIDIIn.options.length = 0;
 
   for (var i=0; i<list.length; i++)
-    if (list[i].name.toString().indexOf("Integra") != -1)
+    if (list[i].name.toString().indexOf("INTEGRA") != -1)
       preferredIndex = i;
 
   if (list.length) {
@@ -65,7 +147,7 @@ function onMIDIInit( midi ) {
   list=midiAccess.outputs();
 
   for (var i=0; i<list.length; i++)
-    if (list[i].name.toString().indexOf("Integra") != -1)
+    if (list[i].name.toString().indexOf("INTEGRA") != -1)
       preferredIndex = i;
 
   if (list.length) {
